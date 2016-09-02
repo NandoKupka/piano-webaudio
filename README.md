f89c46244c459acbd59c2550d430beab6ac76dc7
Fernando Kupka <nandokupka98@gmail.com>
2016-09-02 13:36:23 -0300

    Add jump or slide

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 
 ## About
 
-Simple keyboard with 1 octave (from C4 to C5),hosted it on [Github Pages](https://pages.github.com/).
+Simple keyboard with using WebAudio API sounds, hosted on [Github Pages](https://pages.github.com/).
 
 ## How to Use
 
-Use keyboard keys from ' to = ('1234567890-='), to play the notes and Q to stop the sound
+Use keyboard keys or click to play the notes and `]` to stop the sound
 
 [LET'S ROCK] (https://nandokupka.github.io/piano-webaudio/)
--- main.js
+++ main.js
@@ -27,24 +27,34 @@ $(window).keypress(function (e) {
 });
 var baseAudio = new AudioContext();
 var osc = baseAudio.createOscillator();
+var osc2 = baseAudio.createOscillator();
 var gainNode = baseAudio.createGain();
 osc.connect(baseAudio.destination)
+osc2.connect(baseAudio.destination)
 gainNode.connect(baseAudio.destination);
 osc.connect(gainNode);
-gainNode.gain.value = 0.2;
+gainNode.gain.value = 5;
 osc.frequency.value = 0;
-
 osc.start(0);
 
+
 function keyClick(element) {
-  debugger;
   //press animation
   if (document.getElementsByClassName('pressed')) {
     $(document.getElementsByClassName('pressed')).removeClass('pressed');
   }
-
   $(element).addClass('pressed');
 
   //set frequency
-  osc.frequency.value = parseInt(element.getAttribute('value'));
+  if ($('#select option:selected').attr('value') == "jump") {
+    gainNode.gain.value = -1;
+    setTimeout(function(){ 
+      osc.frequency.value = parseInt(element.getAttribute('value'));
+      gainNode.gain.value = 5;
+    }, 30);
+  }
+  else {
+    debugger;
+    osc.frequency.value = parseInt(element.getAttribute('value'));
+  }
 };
